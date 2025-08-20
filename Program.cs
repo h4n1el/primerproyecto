a5735996c47ff9fff04ae3e461d32ccc115afb36
haniel <hanielsuarez31@gmail.com>
2025-08-20 19:42:34 -0400

    Initial commit of primerproyecto application with basic grading logic and project structure.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace numeracion
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("ingresa un numero, imbecil");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero >= 70 && numero <= 80)
+            {
+                Console.WriteLine(" tu nota es C, jodio etupido");
+            }
+            else if (numero >= 80 && numero <= 90)
+            {
+                Console.WriteLine("cabron tu nota es B");
+            }
+            else if (numero >= 90 && numero <= 100)
+            {
+                Console.WriteLine("tu fucking nota es A mamabicho");
+            }
+
+            Console.ReadKey();
+       
+
+
+        }
+    }
+}
+
