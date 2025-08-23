b4d3c2e00e6b1577fdbb7e72759263863e183187
haniel <hanielsuarez31@gmail.com>
2025-08-23 16:45:41 -0400

    pracita 2#

--- obj/Release/net8.0/primerproyecto.AssemblyInfo.cs
+++ obj/Release/net8.0/primerproyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("primerproyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5735996c47ff9fff04ae3e461d32ccc115afb36")]
 [assembly: System.Reflection.AssemblyProductAttribute("primerproyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("primerproyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
